86d4448cea3b8dabdc88c6ce55081388434c77db
AlejandroSerra-afk <jhusepyt7@gmail.com>
2024-08-06 17:14:42 -0500

    el boton modifica la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar= function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgSaludo");
+    cmpImagenSaludo.src="./imagenes/icegif-1393.gif"
     console.log("Ingresa a saludar");
 }
