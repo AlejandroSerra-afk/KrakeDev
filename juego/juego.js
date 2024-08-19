ce77c4d522866048d4392c709b0cd44763f36d69
AlejandroSerra-afk <jhusepyt7@gmail.com>
2024-08-18 22:11:17 -0500

    se muestra el puntaje del jugador y del computador

--- juego/juego.js
+++ juego/juego.js
@@ -1,3 +1,5 @@
+let puntosUsusario=0;
+let puntosComputador=0;
 
 jugar=function(seleccionado){
     let elemento=generarElemento();
@@ -8,7 +10,11 @@ jugar=function(seleccionado){
         mostrarTexto("lblResultado","EMPATE")
     }else if(numeroGanador==1){
         mostrarTexto("lblResultado","PERDISTE LA PARTIDA!!")
+        puntosComputador=puntosComputador+1;
     }else if(numeroGanador==2){
         mostrarTexto("lblResultado","GANASTE LA PARTIDA!!")
+        puntosUsusario=puntosUsusario+1;
     }
+    mostrarTexto("lblPuntajeUsuario","Puntaje del Usuario: "+puntosUsusario)
+        mostrarTexto("lblPuntajeComputador","Puntaje del Computador: "+puntosComputador)
 }
