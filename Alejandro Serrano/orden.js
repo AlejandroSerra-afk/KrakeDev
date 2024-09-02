52904ae30a92e2256638bd8c555a761928380ccc
AlejandroSerra-afk <jhusepyt7@gmail.com>
2024-09-01 22:38:52 -0500

    Parte3-BUSCAR MAYOR completa

--- Alejandro Serrano/orden.js
+++ Alejandro Serrano/orden.js
@@ -9,12 +9,13 @@ let personas=[
 agregarPersona=function(){
     let nombreR=recuperarTexto("txtNombre");
     let edadR=recuperarTexto("txtEdad");
-    if (validarNombre(nombre) & validarEdad(edad)){
+    if (validarNombre(nombreR) & validarEdad(edadR)){
         let nuevaPersona={};
         nuevaPersona.nombre=nombreR;
         nuevaPersona.edad=edadR;
         personas.push(nuevaPersona);
         alert("PERSONA AGREGADA CORRECTAMENTE ")
+        mostrarPersonas();
     }
 
 }
@@ -36,6 +37,47 @@ mostrarPersonas=function(){
     cmpTabla.innerHTML=contenidoTabla
 }
 
+determinarMayor=function(){
+    let mayor=encontrarMayor();
+    mensaje=mayor.nombre+" con "+mayor.edad;
+    mostrarTexto("lblMayor",mensaje)
+
+}
+
+encontrarMayor=function(){
+    let personaMayor=personas[0];
+    let elementoPersona;
+    for(let i=1;i<personas.length;i++){
+        elementoPersona=personas[i];
+        console.log(elementoPersona.nombre)
+        console.log(elementoPersona.edad)
+        if (elementoPersona.edad>personaMayor.edad){
+            personaMayor=elementoPersona
+        }
+    }
+    return personaMayor;
+}
+determinarMenor=function(){
+    let mayor=encontrarMenor();
+    mensaje=mayor.nombre+" con "+mayor.edad;
+    mostrarTexto("lblMenor",mensaje)
+
+}
+
+encontrarMenor=function(){
+    let personaMenor=personas[0];
+    let elementoPersona;
+    for(let i=1;i<personas.length;i++){
+        elementoPersona=personas[i];
+        console.log(elementoPersona.nombre)
+        console.log(elementoPersona.edad)
+        if (elementoPersona.edad<personaMenor.edad){
+            personaMenor=elementoPersona
+        }
+    }
+    return personaMenor;
+}
+
 validarNombre=function(nombre){
     let tamanio = nombre.length;
     let nombreValido=true;
@@ -55,11 +97,15 @@ validarNombre=function(nombre){
 }
 validarEdad = function (edad) {
     let edadValida = true;
-    if (isNaN(cantidad)) {
+    if (edad==""){
         mostrarTexto("lblErrorEdad", "CAMPO OBLIGATORIO");
         edadValida = false;
     }
-    if (cantidad < 0 || cantidad > 100) {
+    if (isNaN(edad)) {
+        mostrarTexto("lblErrorEdad", "DEBE INGRESAR UN NUMERO");
+        edadValida = false;
+    }
+    if (edad < 0 || edad > 100) {
         mostrarTexto("lblErrorEdad", "LA EDAD DEBE SER UN NUMERO ENTRE 0 Y 100")
         edadValida = false;
     }
